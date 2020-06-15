946c202ccd4b4ba8bf9a64cc18f1eda27140e443
James Ma <james@pop-os.localdomain>
2020-06-14 23:37:08 -0700

    Deploy gh-pages

---
+++ out/_next/static/vK7JaJ3V0Tb6k5PymcEf1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/subpages/[id]":[a,"static\u002Fcss\u002F76110f52ce7389714814.css"]}}("static\u002Fchunks\u002Fa3250bd99b358af3261cd18586f337a684784472.68c94ad7a7f9194ea268.js","static\u002Fcss\u002Fefadfa64c5640075cb35.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
