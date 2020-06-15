226f34897f7e1240eccff99fcfb0421470a79bed
James Ma <james@pop-os.localdomain>
2020-06-14 22:50:52 -0700

    Deploy gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c073dacec78762c5a59a";
+/******/ 	var hotCurrentHash = "98668cce909446dbfc7b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ out/_next/static/swyudK22Lu76N-FijIRpv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/subpages/[id]":[a,"static\u002Fcss\u002F76110f52ce7389714814.css"]}}("static\u002Fchunks\u002F04e0a6bdcba91c0f60c8887ed991a7e4d2c6dc31.68c94ad7a7f9194ea268.js","static\u002Fcss\u002Fefadfa64c5640075cb35.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
