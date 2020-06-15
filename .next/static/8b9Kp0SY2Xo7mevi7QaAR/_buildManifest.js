1d7db98da51a26fa847c4d59b0050e522c4fee0e
James Ma <james@pop-os.localdomain>
2020-06-14 21:59:39 -0700

    edited back json

---
+++ .next/static/8b9Kp0SY2Xo7mevi7QaAR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/subpages/[id]":[a,"static\u002Fcss\u002F76110f52ce7389714814.css"]}}("static\u002Fchunks\u002Ffb6eb482c2168331eddf57b3d16fece8edb6f385.0645e0528375654b4ab1.js","static\u002Fcss\u002Fefadfa64c5640075cb35.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
